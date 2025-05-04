a993345ee4457ce32036acc7585c41509ef14e90
ayushman1210 <ayushmangupta892@gmail.com>
2025-05-04 16:41:11 +0530

    oss

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ port=process.env.PORT
 app.use(express.json())
 
 app.use(cors({
-    origin:'https://registerpage-phi.vercel.app',
+    origin:'https://www.ossrndc.in/',
     methods:["GET","POST"],
     crendentials:true
 }))
