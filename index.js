c3b95601d73e86041283f402548128e594db9052
ayushman1210 <ayushmangupta892@gmail.com>
2025-05-05 17:26:08 +0530

    updated

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ port=process.env.PORT
 app.use(express.json())
 
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin:'https://www.ossrndc.in',
     methods:["GET","POST"],
     crendentials:true
 }))
